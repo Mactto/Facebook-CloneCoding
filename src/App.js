83f6b656eba6c550dbc31e83116e8c2529e9ac2d
mactto <shc9928@gmail.com>
2020-10-02 23:58:43 +0900

    updates

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import Widgets from "./Widgets"
 
 function App() {
+  const user = "mactto";
+
   return (
     <div className="app">
+    {!user ? (
+      <h1>Login</h1>
+    ) : (
+      <>
       <Header />
 
       <div className="app_body">
         <Sidebar />
         <Feed />
+        <Widgets />
       </div>
+      </>
+    )}
     </div>
   )
 }
